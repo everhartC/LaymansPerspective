fd21d3b0e570647667ae58de3d860ad0a8dc7a22
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-07-05 19:11:21 -0700

    Pushing content

--- static/js/home.js
+++ static/js/home.js
@@ -1,12 +1,1 @@
-$(function() {
-    // $('.hero-body').animate({
-    //     transitionDown: '1s'
-    // }, {
-    //     duration: 1,
-    //     complete: function() {
-    //         $('.articles').animate({
-    //             transitionDown: '1s'
-    //         }, 1);
-    //     }
-    // });
-});
+
