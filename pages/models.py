755355bd7d63739c4f85bfb1faa396719a33c746
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-07-05 22:19:11 -0700

    Added resources information and template. This includes 5 books with links to where it can be purchased and 1 short story with link to pdf

--- pages/models.py
+++ pages/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, null=True, blank=True)
     author = models.CharField(max_length=200)
-    publisher = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=13)
+    link = models.URLField(max_length=200, null=True, blank=True)
+    publisher = models.CharField(max_length=200, null=True, blank=True)
+    isbn = models.CharField(max_length=13, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from typing import List
+from django.views.generic import TemplateView, ListView
+from .models import Book
 
 # Create your views here.
 
@@ -8,3 +10,9 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class BookListView(ListView):
+    template_name = 'resources.html'
+    model = Book
+    context_object_name = 'book_list'
