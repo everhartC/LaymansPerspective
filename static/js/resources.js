14d3515d3ea3d437fe617974e3df763cc1ea8ae1
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-07-10 15:25:49 -0700

    Currently displayed as a dropdown but I am about to change to only be a collapsible button

--- static/js/resources.js
+++ static/js/resources.js
@@ -3,4 +3,14 @@ $(function() {
         e.preventDefault();
         window.open($(this).attr("href"), "_blank");
     });
+
+    var $content = $('.dropdown-content');
+    $('.dropdown').on('click', function() {
+        if ($content.css('display') == 'none') {
+            $content.css('display', 'block');
+        } else {
+            $content.hide();
+        }
+    })
+    console.log($content);
 });
