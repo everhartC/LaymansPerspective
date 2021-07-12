8ad91d2973ac2f86789dff486b9088af42593c46
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-07-11 21:20:46 -0700

    A bunch of styling changes and trying new things. Nothing changed in the database.

--- static/js/resources.js
+++ static/js/resources.js
@@ -1,16 +1,9 @@
 $(function() {
-    $('.booklink').on('click', function(e) {
+    $('.accordion').on('click', '.button', function(e) {
         e.preventDefault();
-        window.open($(this).attr("href"), "_blank");
+        $(this)
+            .next().children()
+            .not(':animated')
+            .slideToggle();
     });
-
-    var $content = $('.dropdown-content');
-    $('.dropdown').on('click', function() {
-        if ($content.css('display') == 'none') {
-            $content.css('display', 'block');
-        } else {
-            $content.hide();
-        }
-    })
-    console.log($content);
 });
