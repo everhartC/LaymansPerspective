10b55c53250034db106db4da75a68e4a82cc38e0
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-07-13 00:57:07 -0700

    Trying to make the navbar characters fade in but can't seem to get it to work.

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,65 @@
+$(function() {
+    const text = $(".fancy");
+    const strText = text.text();
+    const re = /\s*(?:;\r\n|\r|\n|$)\s*/;
+    const splitText = strText.split(re);
+    const newStr = shiftPop(splitText);
+    const strArr = newStr.split('');
+    text.text("");
+    // console.log(splitText);
+    // console.log(newStr);
+    // console.log(strArr);
+    // console.log(splitText);
 
+
+    $.each(splitText, function(i, val) {
+        $('#'+i).append("<span>" + val + "</span>");
+    });
+    console.log(splitText.length);
+    
+    let char = 0;
+    let timer = setInterval(onTick, 50);
+    // const span;
+
+
+    // function onTick(){
+    //     const span = $('.fancy').find('span')[char];
+        // console.log(span);
+
+        // span.addClass('fade');
+        // // return console.log(span);
+        // // span.classList.add('fade');
+        // char++;
+        // if(char === splitText.length){
+        //     complete();
+        //     return;
+        // };
+    // };
+    
+    // function complete() {
+    //     clearInterval(timer);
+    //     timer = null;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function shiftPop(arr) {
+        arr.shift();
+        arr.pop();
+        var newStr = arr.join('');
+        newStr = newStr.split(' ');
+        newStr = newStr.join('');
+        return newStr;
+    }
+});
