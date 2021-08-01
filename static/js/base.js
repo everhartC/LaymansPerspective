ef468f6551647d8f6db7560d9d15b62b32e6a6e2
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-07-31 21:37:48 -0700

    Added toggleable mobile functionality to navbar

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,13 @@
 $(function() {
-    var $html = $('html');
-    $html.attr('id', 'htmlaccess');
-})
+    // var $html = $('html');
+    // $html.attr('id', 'htmlaccess');
+
+    // Allow expandable burger on mobile
+    $('.navbar-burger').on('click', function() {
+        $('.navbar-burger').toggle('is-active');
+        $('.navbar-menu').toggle('is-active');
+        $('.navbar-burger').toggle('open');
+    });
+
+});
 
