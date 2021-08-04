ddd3de14e74217d5f67de3271a750cdc78923648
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-08-04 14:14:22 -0700

    Fixed DATABASE_URL env var

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.dj_db_url("DATABSE_URL"),
+    'default': env.dj_db_url("DATABASE_URL"),
     # {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
