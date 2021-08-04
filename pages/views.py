654e51a1aefdb59c5ccf57387e8b693c4bbe1d9f
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-08-03 21:48:39 -0700

    Have the form currently reload the page but it still doesn't add email to mail list

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,26 @@
-from typing import List
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, ListView
-from .models import Book, Review
+from .models import Book, Review, MailList
 from .forms import EmailForm
 
 
 # Create your views here.
 
 class HomePageView(TemplateView):
+    model = MailList
     template_name = 'home.html'
+
+    form_class = EmailForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
