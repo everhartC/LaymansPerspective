b53a36d68de9e5c4c0bfbc30ed0903d691ac8d42
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-08-04 14:35:47 -0700

    Changed db url settings

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-import dj_database_url
 
 DATABASES = {
     'default': {
@@ -93,7 +92,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = env.dj_db_url("DATABASE_URL")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
