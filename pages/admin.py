87e4166ffd50adc9bfbb2a54fcb6a42362369233
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-08-04 15:50:44 -0700

    Added all models to admin site

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, MailList, Review, Question
+admin.site.register(Book)
+admin.site.register(MailList)
+admin.site.register(Review)
+admin.site.register(Question)
