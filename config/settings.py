2aac97d655fedb79700b938e7d669c0aa5e89dd2
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-08-04 14:32:57 -0700

    Experimenting with dj_database_url

--- config/settings.py
+++ config/settings.py
@@ -85,14 +85,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': env.dj_db_url("DATABASE_URL"),
-    # {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
