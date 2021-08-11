d7a0ebf9561e71027d4da93997296fcba5fc8a85
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-08-10 22:10:18 -0700

    About Dave back to original working state

--- static/js/about.js
+++ static/js/about.js
@@ -1,4 +1,8 @@
 $(function() {
     var $html = $('html');
     $html.attr('id', 'aboutHtml');
+
+    $('.hero').offset({top: $('.navbar').innerHeight(), left: 0});
+    console.log($('.hero').position());
+    console.log($('.hero').offset());
 })
