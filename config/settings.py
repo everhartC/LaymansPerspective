d65b1553283fa607b1939cf6d2079dadccb6465f
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-09-16 21:32:36 -0700

    Reverting back to sqlite3 for development

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['www.camboweb.dev','.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.camboweb.dev','laymans-perspective.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -87,15 +87,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     # "default": env.dj_db_url("DATABASE_URL")
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Charlie99!9',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Charlie99!9',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -150,3 +157,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
